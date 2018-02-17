8cbac1e5901739f829438d93236808b47bfea47b
Judah Hunger <judah.hunger@gmail.com>
2018-02-16 17:05:04 -0800

    added bg image and helped with style - also started trying to find a event for checking boxes and changing look of text

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var checkbox = document.querySelector('input[name=checkbox]');
+
+checkbox.addEventListener( 'change', function() {
+  if(this.checked) {
+   
+  } else {
+    // Checkbox is not checked..
+  }
+});
